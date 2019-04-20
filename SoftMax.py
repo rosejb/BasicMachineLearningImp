2099e9c7b13c9eefb2ce219d12978f2611beb444
James Rose <james.b.rose@vanderbilt.edu>
2019-04-20 14:09:45 -0500

    Utilized L-BFGS-B from scipy.optimize.minimize to train softmax regression. Added basic lambda selection based on cross validation data.

--- SoftMax.py
+++ SoftMax.py
@@ -5,14 +5,17 @@ from scipy.optimize import minimize, check_grad
 class SoftMaxRegression(object):
     def __init__(self, x_mat, y_vals, k, lamb, needs_bias_term=False):
 
-        self.y_vals= y_vals
+        self.y_vals = y_vals
         self.k = k
         self.lamb = lamb
+        self.theta = np.zeros((x_mat.shape[1], k))
 
         if needs_bias_term:
             self.x_mat = np.concatenate((np.zeros((self.x_mat.shape[0], 1)), x_mat), axis=1)
         else:
             self.x_mat = x_mat
+
+        self.train(self.theta)
 
     def softmax_cost(self, theta):
         theta = theta.reshape((self.x_mat.shape[1], self.k), order='F')
@@ -45,16 +48,31 @@ class SoftMaxRegression(object):
 
         return jac.flatten('F')
 
+    def train(self):
+        theta = np.zeros(self.x_mat.shape[1], self.k)
 
+        result = minimize(self.softmax_cost, theta, method='L-BFGS-B', jac=self.softmax_grad,
+                          options={'gtol':1e-5})
 
-# def train_softmax(X, y, k, lamb):
-#     theta = np.zeros(X.shape[1] * k)
-#
-#     opt_sc = lambda theta: softmax_cost(theta, X, y, k, lamb)
-#     opt_jac = lambda theta: softmax_grad(theta, X, y, k, lamb)
-#
-#
-#     result = minimize(opt_sc, theta, method='L-BFGS-B', jac=opt_jac,
-#                       options={'gtol':1e-5})
-#
-#     return result.x.reshape((X.shape[1], k), order='F')
+        self.theta = result.x.reshape((self.x_mat.shape[1], self.k), order='F')
+
+    def predict(self, x):
+        return np.argmax(x@self.theta)
+
+    def fit_lambda(self, cv_feature_mat, cv_labels):
+        max_cv_acc = 0
+        max_lamb = .01
+
+        for l in [.01 * 2**i for i in range(8)]:
+            self.lamb = l
+            self.train()
+
+            cv_predict = np.argmax(cv_feature_mat@self.theta, axis=1)
+            cv_accuracy = np.equals(cv_predict.flatten(), cv_labels.flatten()).mean()
+            if cv_accuracy > max_cv_acc:
+                max_lamb = l
+                max_cv_acc = cv_accuracy
+
+        self.lamb = max_lamb
+        return max_lamb, max_cv_acc
+
